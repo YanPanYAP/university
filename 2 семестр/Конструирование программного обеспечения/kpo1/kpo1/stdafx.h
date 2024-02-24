fd484194b7c186ca05568a499311269bf0770c1f
Yan <151462964+YanPanYAP@users.noreply.github.com>
2024-02-24 19:40:14 +0300

    Add files via upload

---
+++ 2 семестр/Конструирование программного обеспечения/kpo1/kpo1/stdafx.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <string>
+#include <iostream>
+using namespace std;
+
+bool isCorrect(int day, int month, int year, string date);
+bool isLeapYear(int year);
+int getDaysToBirthday(int day, int month, int year);
+int getDayNumber(int day, int month, int year);
