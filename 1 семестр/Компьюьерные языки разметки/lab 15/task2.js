02cdddf93819545b6d720217610cadc45d695587
Yan <151462964+YanPanYAP@users.noreply.github.com>
2024-02-24 19:55:55 +0300

    Delete 1 семестр/Компьюьерные языки разметки directory

--- 1 семестр/Компьюьерные языки разметки/lab 15/task2.js
+++
@@ -1,7 +0,0 @@
-let form = document.querySelector("form");
-let footer = document.querySelector("footer");
-
-
-function fn(){
-    footer.innerHTML = form.elements[0].value+ " " +form.elements[1].value+ " " +form.elements[2].value+ " " +form.elements[3].value+ " " +form.elements[4].value+ " " +form.elements[5].value;
-}
